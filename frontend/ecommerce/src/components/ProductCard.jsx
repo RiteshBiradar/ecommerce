3c15ef323beb2bf065108d9707ff38be6c9855c5
Ritesh Biradar <riteshbiradar3434@gmail.com>
2025-05-08 12:01:28 +0530

    fix : modified Product Card, fixed image size

--- frontend/ecommerce/src/components/ProductCard.jsx
+++ frontend/ecommerce/src/components/ProductCard.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const ProductCard = ({ image, name, price, description, addedTime }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full" src={image} alt="Product" />
+    <div className="w-full aspect-[4/3] overflow-hidden rounded-t-md">
+      <img
+        src={image}
+        alt="Product"
+        className="w-full h-full object-cover"
+      />
+    </div>
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{name}</div>
         <p className="text-gray-700 text-base">{description}</p>
